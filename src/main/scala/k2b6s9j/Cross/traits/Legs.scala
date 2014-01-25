543c5756c7f7f512e4e452f24b4c4faa0d680750
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-24 18:59:27 -0700

    Value not Variable.

--- src/main/scala/k2b6s9j/Cross/traits/Legs.scala
+++ src/main/scala/k2b6s9j/Cross/traits/Legs.scala
@@ -2,29 +2,29 @@ package k2b6s9j.Cross.traits
 
 trait Legs {
   
-  var amount: Int = 0
+  val amount: Int = 0
 
   trait Biped extends Legs {
 
-    override var amount: Int = 2
+    override val amount: Int = 2
 
   }
 
   trait Quadruped extends Legs {
 
-    override var amount: Int = 4
+    override val amount: Int = 4
 
   }
 
   trait Hectaped extends Legs {
 
-    override var amount: Int = 6
+    override val amount: Int = 6
 
   }
 
   trait Octoped extends Legs {
 
-    override var amount: Int = 8
+    override val amount: Int = 8
     
   }
 
