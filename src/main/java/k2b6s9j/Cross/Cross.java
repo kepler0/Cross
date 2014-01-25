b04045801f90b25ceedd990ae2a6509933decb60
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-24 18:23:29 -0700

    Mod will start in Java.

---
+++ src/main/java/k2b6s9j/Cross/Cross.java
@@ -0,0 +1,30 @@
+package k2b6s9j.Cross;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "Cross", name = "Cross", version = "1.0")
+public class Cross {
+
+    @Mod.Instance("Cross")
+    public static Cross instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void initialize(FMLInitializationEvent event) {
+
+    }
+
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+}
