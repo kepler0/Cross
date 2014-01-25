b6e4d1fd8818a18b5029c9e1ea89209798d5048e
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-01-24 19:08:59 -0700

    Print mod info.

--- src/main/java/k2b6s9j/Cross/Cross.java
+++ src/main/java/k2b6s9j/Cross/Cross.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = "Cross", name = "Cross", version = "1.0")
 public class Cross {
@@ -11,9 +12,18 @@ public class Cross {
     @Mod.Instance("Cross")
     public static Cross instance;
 
+    public static Logger log;
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
+        this.log = event.getModLog();
 
+        PrintModInfo();
+    }
+
+    private void PrintModInfo() {
+        log.info("Cross");
+        log.info("Copyright Kepler Sticka-Jones 2014");
     }
 
     @Mod.EventHandler
